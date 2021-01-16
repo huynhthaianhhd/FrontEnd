f2eace57e99dcaf2167344b246c03a8fa3607f97
Lem <nguyenhuugiatri@gmail.com>
2021-01-16 17:41:36 +0700

    enhance handle book and ui

--- src/app/containers/Booking/hooks.js
+++ src/app/containers/Booking/hooks.js
@@ -1,46 +1,70 @@
 import useActions from 'hooks/useActions';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
   selectBookingData,
   selectBookingStatus,
   selectBookingSeats,
   selectBookingPickedSeats,
+  selectBookingBookStatus,
 } from './selectors';
 import { actions } from './slice';
 import { ACTION_STATUS } from 'utils/constants';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import get from 'lodash/fp/get';
+import moment from 'moment';
 
 const useHooks = () => {
   const { showTimeId } = useParams();
+  const history = useHistory();
   const status = useSelector(selectBookingStatus);
+  const bookStatus = useSelector(selectBookingBookStatus);
   const seats = useSelector(selectBookingSeats);
   const pickedSeats = useSelector(selectBookingPickedSeats);
   const data = useSelector(selectBookingData);
+  const movie = get('showTime[0].movie', data);
+  const startTime = moment(get('showTime[0].startTime', data)).format(
+    'YYYY/MM/DD HH:mm',
+  );
+  const [paymentMethod, setPaymentMethod] = useState(1);
 
-  const { getBookingInfo, updateSeatStatus, book } = useActions(
+  const { resetData, getBookingInfo, updateSeatStatus, book } = useActions(
     {
       getBookingInfo: actions.getBookingInfo,
       updateSeatStatus: actions.updateSeatStatus,
       book: actions.book,
+      resetData: actions.resetData,
     },
     [actions],
   );
 
   useEffect(() => getBookingInfo(showTimeId), [getBookingInfo, showTimeId]);
 
+  useEffect(() => () => resetData(), [resetData]);
+
+  useEffect(() => {
+    if (bookStatus === ACTION_STATUS.SUCCESS) history.push('/');
+  }, [bookStatus, history, resetData]);
+
   const handleClickSeat = ({ id, newStatus }) =>
     updateSeatStatus({ id, newStatus });
 
   const handleBook = () => book({ showTimeId, pickedSeats });
 
+  const handleChangeRadio = e => {
+    setPaymentMethod(e.target.value);
+  };
+
   return {
-    handlers: { handleClickSeat, handleBook },
+    handlers: { handleClickSeat, handleBook, handleChangeRadio },
     selectors: {
       loading: status === ACTION_STATUS.PENDING,
       data,
       seats,
       pickedSeats,
+      movie,
+      startTime,
+      paymentMethod,
     },
   };
 };
--- src/app/containers/Booking/styles/index.js
+++ src/app/containers/Booking/styles/index.js
@@ -1,4 +1,116 @@
-import styled, { css } from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'app/components/Button';
+import styled from 'styled-components';
 
 export const StyledBooking = styled.div``;
+
+export const Price = styled.h1`
+  color: #44c020;
+  font-size: 40px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+export const Border = styled.div`
+  width: 100%;
+  border-bottom: 1px dashed #d4d4d4;
+  margin: 20px 0;
+`;
+
+export const PickedSeats = styled.div`
+  display: flex;
+  justify-content: space-between;
+  h4 {
+    font-size: 16px;
+    font-weight: 500;
+    color: #44c020;
+  }
+  h3 {
+    font-size: 17px;
+    font-weight: 550;
+  }
+`;
+
+export const Payments = styled.div`
+  h3 {
+    font-size: 17px;
+    font-weight: 550;
+  }
+  .ant-radio-wrapper {
+    padding-left: 20px;
+    font-size: 15px;
+    font-weight: 500;
+  }
+`;
+
+export const BookingButton = styled(Button)`
+  transition: none;
+  font-size: 20px;
+  background-image: linear-gradient(223deg, #b4ec51 0, #429321 100%);
+  height: 60px;
+  color: white;
+  border: none;
+
+  &:hover {
+    background-image: linear-gradient(223deg, #b4ec51 0, #429321 100%);
+    color: white;
+    opacity: 0.8;
+  }
+
+  &:focus {
+    background-image: linear-gradient(223deg, #b4ec51 0, #429321 100%);
+    color: white;
+    opacity: 0.8;
+  }
+
+  &.ant-btn[disabled] {
+    color: white;
+    opacity: 1;
+    background-color: #afafaf;
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+export const SideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+  height: calc(100vh - 64px);
+  .content {
+    padding: 40px;
+  }
+`;
+
+export const MovieName = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0;
+`;
+
+export const MovieInfo = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    display: inline-block;
+    width: 70px;
+    margin-right: 15px;
+  }
+`;
+
+export const VerticalRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HorizontalRow = styled.div`
+  display: flex;
+  align-items: center;
+  p {
+    margin: 0;
+    font-size: 15px;
+    line-height: 28px;
+  }
+`;
